029c1e2b4463513c204c0cf98d7e9c3f71e81d6e
Sodjiro <kadyr91@yahoo.com>
2019-09-06 12:57:13 +0300

    finl

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,18 @@
 
-pollutantmean<-function(directory,pollutant,id){
+pollutantmean<-function(directory,pollutant,id=1:332){
+  
   allfiles<-list.files(paste(getwd(),"/",directory, sep = ""))
-  files<-allfiles[id,]
+  x<-c()
+  y<-c()
   for(i in id){
-    path<-paste(getwd(),"/",directory,"/",files[i], sep = "")
-  x<-read.csv(path)[[pollutant]]
-  allvalues<-cbind(allvalues,x)
+    path<-paste(getwd(),"/",directory,"/",allfiles[i], sep = "")
+    y<-sum(na.omit(read.csv(path)[[pollutant]]),y)
+    x<-sum(length(na.omit(read.csv(path)[[pollutant]])),x)
   }
-  mean(allvalues)
+  y/x
+  
+  # mean(x,na.rm = TRUE)
+  # allvalues<-merge(x[0],x[i])
+  # nrow(allvalues)
+  ## mean(allvalues[[pollutant]],na.rm = TRUE)
 }
